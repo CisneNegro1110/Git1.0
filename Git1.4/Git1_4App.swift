bb8bc8bacda0e84197033abd6bd28f7b8295935e
JesúsFranciscoLeyva <leyva527@gmail.com>
2021-08-06 17:30:10 -0600

    Initial Commit

---
+++ Git1.4/Git1_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Git1_4App.swift
+//  Git1.4
+//
+//  Created by Jesús Francisco Leyva Juárez on 06/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Git1_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
