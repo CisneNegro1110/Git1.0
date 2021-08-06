b648cf56afeaa6681d300cf9daf601d48e6ec950
JesúsFranciscoLeyva <leyva527@gmail.com>
2021-08-06 17:09:02 -0600

    choose a side with image

--- Git1.0/ContentView.swift
+++ Git1.0/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedSide: SideOfTheImage = .alien
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Picker("Choose a Side", selection: $selectedSide){
+                    ForEach(SideOfTheImage.allCases, id: \.self) {
+                        Text($0.rawValue)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
+                Spacer()
+                ChooseItem(selectedSide: selectedSide)
+                Spacer()
+            }
+            .navigationTitle("Choose a Side")
+        }
     }
 }
 
@@ -19,3 +33,35 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+enum SideOfTheImage: String, CaseIterable {
+    case alien = "Alien"
+    case astronauta = "Astronauta"
+    case weed = "Weed"
+}
+
+struct ChooseItem: View {
+    var selectedSide: SideOfTheImage
+    
+    var body: some View {
+        switch selectedSide {
+        case .alien:
+            ImageView(imageNameItem: "alien")
+        case .astronauta:
+            ImageView(imageNameItem: "astronauta")
+        case .weed:
+            ImageView(imageNameItem: "weed")
+        }
+    }
+}
+
+struct ImageView: View {
+    var imageNameItem: String
+    var body: some View {
+        Image(imageNameItem)
+            .resizable()
+            .frame(width: 250, height: 400)
+            .shadow(color: .white, radius: 100)
+    }
+}
+
